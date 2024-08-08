08619db3476249c29aafbf2920a49b4cd5161ba1
bao zhuhan <baozhuhan2004@163.com>
2024-08-08 23:11:59 +0800

    update java class , update system files tree

---
+++ JAVA/JavaClass/JavaCLassMethod/Main.java
@@ -0,0 +1,11 @@
+package JAVA.JavaClass.JavaCLassMethod;
+
+public class Main {
+    public void fullThrottle(){
+        System.out.println("The car is going as fast as it can");
+    }
+
+    public void speed(int speed) {
+        System.out.println("Speed is : " + speed);
+    }
+}
---
+++ JAVA/JavaClass/JavaCLassMethod/Second.java
@@ -0,0 +1,9 @@
+package JAVA.JavaClass.JavaCLassMethod;
+
+public class Second {
+    public static void main(String[] args){
+        Main myCar = new Main();
+        myCar.fullThrottle();
+        myCar.speed(200);
+    }
+}
